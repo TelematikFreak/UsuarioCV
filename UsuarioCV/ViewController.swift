73a53060d164145b47791bc1462dc1ab88bef792
Alex Moreno <alx.spain@gmail.com>
2019-12-08 15:21:28 +0000

    Initial Commit

---
+++ UsuarioCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsuarioCV
+//
+//  Created by Alexander Moreno Guillén on 8/12/19.
+//  Copyright © 2019 Alexander Moreno Guillén. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
